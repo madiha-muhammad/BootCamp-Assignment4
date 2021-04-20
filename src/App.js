969ecbe2a76d9492e3f41f4eb195c7bef1cd2ca9
madiha <78408471+madiha-muhammad@users.noreply.github.com>
2021-04-20 06:15:28 +0500

    Hoocks,

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React,{useState}  from 'react';
+import React, { useState } from 'react';
+import { Message } from './message.js';
 import './App.css';
 
 function App() {
   let [count, setCount] = useState(0);
+  let [isMorning, setMorning] = useState(true);
 
   return (
-    <div>
-      <h5>Value of counter variable: {count}</h5>
+    <div className={`box ${isMorning ? 'dayLight' : ''}`}>
+      <h4>Day Time = {isMorning ? 'Morning' : 'Evening'}</h4>
+
+      <Message counter={count}/>
+      <br/>
+      <button onClick={
+        () => setCount(++count)
+        }>
+          Update Counter
+      </button>
+
+      <button onClick = {
+        () => setMorning(!isMorning)}>Update Day</button>
     </div>
   );
 }
